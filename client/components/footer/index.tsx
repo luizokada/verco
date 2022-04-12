e7d98719a24b8934431fdd73c7de7df3699213c7
Sarah Anduca <sarah.anduca@eurekalabs.com.br>
2022-04-12 20:38:33 -0300

    make footer

--- client/components/footer/index.tsx
+++ client/components/footer/index.tsx
@@ -1,13 +1,59 @@
-import { Container, Text } from "@nextui-org/react";
+import { Container, Text, Grid } from "@nextui-org/react";
 import GreenLogo from "../../public/logo-green.svg";
 
-export const Footer = ({}) => (
-  <Container
-    display="flex"
-    alignContent="flex-end"
-    css={{ background: "$brown", color: "$textSecondary" }}
-  >
-    <GreenLogo />
-    <Text>Footer</Text>
-  </Container>
-);
+function TextDisplay({
+  title,
+  textList,
+}: {
+  title: string;
+  textList: string[];
+}) {
+  return (
+    <Grid
+      xs={2}
+      css={{ display: "flex", flexDirection: "column", color: "$textPrimary" }}
+    >
+      <Text css={{ fontSize: 24, color: "$textPrimary", fontFamily: "$title" }}>
+        {title}
+      </Text>
+      {textList.map((text) => (
+        <Text
+          css={{
+            fontSize: 14,
+            color: "$textSecondary",
+            fontFamily: "$text",
+          }}
+        >
+          {text}
+        </Text>
+      ))}
+    </Grid>
+  );
+}
+
+export const Footer = ({}) => {
+  const institucional = [
+    "Sobre a loja",
+    "Política de entrega",
+    "Política de condições",
+    "Política de privacidade",
+  ];
+  const duvidas = ["Fale conosco", "Dúvidas Frequentes"];
+  const minhaConta = ["Meus pedidos", "Endereço de entrega", "Alterar senha"];
+  return (
+    <Container
+      display="flex"
+      alignContent="flex-end"
+      css={{ background: "$brown", color: "$textSecondary", padding: "30px" }}
+    >
+      <Grid.Container gap={1} justify="center">
+        <Grid xs={4}>
+          <GreenLogo />
+        </Grid>
+        <TextDisplay title="Institucional" textList={institucional} />
+        <TextDisplay title="Dúvidas" textList={duvidas} />
+        <TextDisplay title="Minha Conta" textList={minhaConta} />
+      </Grid.Container>
+    </Container>
+  );
+};
